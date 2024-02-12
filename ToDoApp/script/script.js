22b52dbda90d3402669b86e690b6dbc4f88f7ad0
Gabi Dragan <dragangabriel40@yahoo.com>
2024-02-12 19:57:42 +0200

    .

---
+++ ToDoApp/script/script.js
@@ -0,0 +1,11 @@
+const task = document.getElementsByClassName("taskList");
+let getTask = "";
+
+function getInput() {
+  const getTask = document.getElementsByClassName("introducedTask").value;
+  return getTask;
+}
+function addTask() {
+    getInput();
+
+}
